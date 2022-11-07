6877f3645fa5a751d8c9caf8fde71cdd4f96ca1a
ianhorsburgh <nickh91101@gmail.com>
2022-11-06 19:36:35 -0800

    new settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -131,7 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'projects/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Default primary key field type
